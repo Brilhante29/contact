79ed06f58a317fc897efcd74c134b721e98e0fc2
Guilherme Brilhante <85261647+Brilhante29@users.noreply.github.com>
2023-08-26 12:11:14 -0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,32 @@
 provider "aws" {
-  region = "us-east-2"
+  region     = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
 }
 
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP inbound traffic"
+  description = "Allow SSH and HTTP"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 22
@@ -35,79 +41,72 @@ resource "aws_security_group" "allow_ssh_http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  vpc_id = aws_vpc.default.id
-}
-
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_internet_gateway" "default" {
-  vpc_id = aws_vpc.default.id
-}
-
-resource "aws_route_table" "default" {
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.default.id
-  }
-}
-
-resource "aws_main_route_table_association" "default" {
-  vpc_id         = aws_vpc.default.id
-  route_table_id = aws_route_table.default.id
-}
-
-resource "aws_subnet" "default" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.0.0/24"
-  map_public_ip_on_launch = true
 }
 
 resource "aws_instance" "app_server" {
-  ami             = "ami-0abcd1234efgh5678"
-  instance_type   = "t2.micro"
-  key_name        = "app-ssh-key"
-  subnet_id       = aws_subnet.default.id
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = var.key_name
   security_groups = [aws_security_group.allow_ssh_http.name]
 
   tags = {
     Name = var.ec2_name
   }
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("~/.ssh/app-ssh-key.pem")
-    host        = self.public_ip
-  }
-
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install -y docker.io",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
       "sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "git clone https://github.com/Brilhante29/contact.git",
       "cd contact",
-      "sudo docker-compose up -d",
+      "docker-compose up --build -d"
     ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = var.private_key
+      host        = self.public_ip
+    }
   }
 }
 
+output "instance_id" {
+  value = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+variable "aws_access_key" {
+  description = "The AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name to use for the EC2 instance"
+  type        = string
+}
+
 variable "ec2_name" {
-  type = string
+  description = "The name to use for the EC2 instance"
+  type        = string
+}
+
+variable "private_key" {
+  description = "The private key to use for the EC2 instance"
+  type        = string
+}
+
+variable "repository_url" {
+  description = "The URL of the GitHub repository to clone"
+  type        = string
 }
